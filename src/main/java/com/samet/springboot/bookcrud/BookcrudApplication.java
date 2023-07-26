7115f3ce427b93a284992fd73bddb40a09a48253
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-07-26 15:21:41 +0300

    Initial commit

---
+++ src/main/java/com/samet/springboot/bookcrud/BookcrudApplication.java
@@ -0,0 +1,13 @@
+package com.samet.springboot.bookcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samet/springboot/bookcrud/BookcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samet.springboot.bookcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
