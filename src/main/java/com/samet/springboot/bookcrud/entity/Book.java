cafb436acdaaf2994d4167840c0eae511cfd72d8
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-07-26 15:27:20 +0300

    Create Entity Class

---
+++ src/main/java/com/samet/springboot/bookcrud/entity/Book.java
@@ -0,0 +1,54 @@
+package com.samet.springboot.bookcrud.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "writer")
+    private String writer;
+
+    public Book(){
+
+
+    }
+
+    public Book(String title, String writer) {
+        this.title = title;
+        this.writer = writer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+}
